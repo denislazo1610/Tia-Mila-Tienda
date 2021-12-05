cee65780bc625f2101c3d92cc8d8b8541d5a8a15
Denis Lazo <denislazo1610@gmail.com>
2021-12-04 21:25:30 -0700

    fixing some bugs in js

--- tienda/tienda.js
+++ tienda/tienda.js
@@ -11,6 +11,18 @@ const clothes = {
     color: "blue",
     location: "../galeria/clothes/prenda_azul.jpeg",
   },
+  prendaRoja: {
+    name: "Prenda Roja",
+    price: "6$",
+    color: "blue",
+    location: "../galeria/clothes/prenda_roja.jpeg",
+  },
+  prendaRosada: {
+    name: "Prenda Rosada con Flores",
+    price: "6$",
+    color: "blue",
+    location: "../galeria/clothes/prenda_rosada_con_flores.jpeg",
+  },
 };
 
 const prendas = document.querySelector(".prendas");
@@ -19,6 +31,7 @@ for (let [name, values] of Object.entries(clothes)) {
   const image = document.createElement("img");
 
   image.src = values.location;
+  image.alt = values.name;
 
   prendas.appendChild(image);
 
