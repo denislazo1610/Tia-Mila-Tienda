78f85d120964a5dbced4c2a6d24b84c0037679a3
Denis <denislazo1610@gmail.com>
2021-12-05 17:19:59 -0700

    Add new clothes

--- tienda/tienda.js
+++ tienda/tienda.js
@@ -1,27 +1,111 @@
 const clothes = {
-  yellowBiker: {
-    name: "Yellow Biker",
-    price: "2$",
-    color: "yellow",
+  prenda1: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
+    location: "../galeria/clothes/flor_ombligo.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
+  },
+  prenda2: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
+    location: "../galeria/clothes/flor_vestido.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
+  },
+  prenda3: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
+    location: "../galeria/clothes/jogger_rojo.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
+  },
+  prenda4: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
+    location: "../galeria/clothes/ombligo_rayas.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
+  },
+  prenda5: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
     location: "../galeria/clothes/outfit_yellow_bicker.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
   },
-  prendaAzul: {
-    name: "Prenda Azul",
-    price: "6$",
-    color: "blue",
+  prenda6: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
     location: "../galeria/clothes/prenda_azul.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
   },
-  prendaRoja: {
-    name: "Prenda Roja",
-    price: "6$",
-    color: "blue",
+  prenda7: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
     location: "../galeria/clothes/prenda_roja.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
   },
-  prendaRosada: {
-    name: "Prenda Rosada con Flores",
-    price: "6$",
-    color: "blue",
+  prenda8: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
     location: "../galeria/clothes/prenda_rosada_con_flores.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
+  },
+  prenda9: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
+    location: "../galeria/clothes/punto_0manga_prenda.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
+  },
+  prenda10: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
+    location: "../galeria/clothes/Rojo_un_brazo.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
+  },
+  prenda11: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
+    location: "../galeria/clothes/rosado_fuerte_ombligo.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
+  },
+  prenda12: {
+    name: "Flor Ombligo",
+    price: "$",
+    color: "",
+    location: "../galeria/clothes/rosado_princesa.jpeg",
+    category: "",
+    nuevo: false,
+    discount: 0
   },
 };
 
