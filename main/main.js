8c8c02298c171f2154e86688cd5a68bf5fdad5e1
Denis Lazo <denislazo1610@gmail.com>
2021-12-12 00:09:01 -0700

    Adding Instagram icon

--- main/main.js
+++ main/main.js
@@ -24,6 +24,17 @@ for (let [name, values] of Object.entries(clothes)) {
     carouselPhoto.alt = values.name;
     carouselPhoto.src = values.location;
 
+    carouselPhoto.addEventListener("click", function () {
+      descriptionOuter.classList.toggle("invisible");
+      ejemploPrenda.src = values.location;
+      ejemploPrenda.style.width = "160px";
+      ejemploPrenda.style.height = "210px";
+      ejemploPrenda.style.margin = "0 25%";
+
+      ejemploClotheName.innerHTML = values.name;
+      ejemploPrice.innerHTML = values.price;
+    });
+
     carouselItem.appendChild(carouselPhoto);
     carousel.appendChild(carouselItem);
   }
