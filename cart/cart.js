98f234af569e3fe6e817fdfd2e5233f0ab918914
Denis Lazo <denislazo1610@gmail.com>
2021-12-14 23:03:20 -0700

    Cart an remove clothes, only missing image

--- cart/cart.js
+++ cart/cart.js
@@ -9,15 +9,32 @@ if (!localStorage.length) {
   vacio.style.height = "300px";
   itemsCart.appendChild(vacio);
 } else {
+  checkingLocalstorage();
+  const total = document.createElement("section");
+  const oracion = document.createElement("h2");
+  oracion.style.textAlign = "center";
+  oracion.innerHTML = "Your total:";
+  const totalNumber = document.createElement("span");
+  totalNumber.innerHTML = sum + "$";
+  oracion.appendChild(totalNumber);
+  total.appendChild(oracion);
+  itemsCart.appendChild(total);
+}
+
+function checkingLocalstorage() {
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     const infoClothe = JSON.parse(localStorage.getItem(key));
 
     const itemPart = document.createElement("section");
     itemPart.style.margin = "0 auto 1rem";
+    itemPart.style.paddingBottom = "1rem";
     itemPart.style.width = "80%";
     itemPart.style.border = "solid black";
     itemPart.style.textAlign = "center";
+
+    const buttonCloses = document.createElement("button");
+    buttonCloses.innerHTML = "Remove";
     const name = document.createElement("h3");
     const quantity = document.createElement("h4");
     quantity.style.margin = "0 auto";
@@ -33,6 +50,11 @@ if (!localStorage.length) {
 
     price.innerHTML = "Price per unit: " + infoClothe.price + "$";
 
+    itemPart.appendChild(buttonCloses);
+    buttonCloses.addEventListener("click", function () {
+      localStorage.removeItem(localStorage.key(i));
+      location.reload();
+    });
     itemPart.appendChild(name);
     itemPart.appendChild(quantity);
     itemPart.appendChild(price);
@@ -42,14 +64,4 @@ if (!localStorage.length) {
     const precio = Number(infoClothe.price);
     sum += precio * cantidad;
   }
-
-  const total = document.createElement("section");
-  const oracion = document.createElement("h2");
-  oracion.style.textAlign = "center";
-  oracion.innerHTML = "Your total:";
-  const totalNumber = document.createElement("span");
-  totalNumber.innerHTML = sum + "$";
-  oracion.appendChild(totalNumber);
-  total.appendChild(oracion);
-  itemsCart.appendChild(total);
 }
